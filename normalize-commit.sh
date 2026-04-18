eaf58102e900be5e166eca136c8cb774d1183a1c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-04-18 18:07:42 +0900

    フィルタ適用後にリモートを再設定

--- normalize-commit.sh
+++ normalize-commit.sh
@@ -26,17 +26,53 @@ local  _git=${GIT:-"${git_bin_default}"}
 local  _user_email=${USER_EMAIL:-"${user_email_default}"}
 local  _user_name=${USER_NAME:-"${user_name_default}"}
 
-gitlab_hostname="gitlab.com${url_host_postname}"
-bucket_hostname="bucket.org${url_host_postname}"
+local  _gitlab_hostname="gitlab.com${url_host_postname}"
+local  _bucket_hostname="bucket.org${url_host_postname}"
 
 
-##########################################################################
+##
+##    引数解析
+##
+
+local  _repo_name
+local  _proj_name
+local  _url_prefix
+
+if [[ $# -ge 1 ]] ; then
+    _repo_name=$1
+else
+    _repo_name=''
+fi
+
+if [[ $# -ge 2 ]] ; then
+    _proj_name=$2
+    _url_prefix="${url_account_name}-${_proj_name}"
+else
+    _proj_name=''
+    _url_prefix="${url_account_name}"
+fi
+
+
+##
+##    リポジトリにフィルタを適用
+##
+
+_gitlab_root="git@${_gitlab_hostname}:${_url_prefix}"
+_bucket_root="git@${_bucket_hostname}:${_url_prefix}"
 
 "${_git}"  config --local  'user.email'  "${_user_email}"
 "${_git}"  config --local  'user.name'   "${_user_name}"
 
 "${_git}"  filter-repo  --email-callback "  return b'${_user_email}'"
 "${_git}"  filter-repo  --name-callback  "  return b'${_user_name}'"
+
+
+# 解除されるリモートを再設定
+
+if [[ "X${_repo_name}Y" != 'XY' ]] ; then
+    "${_git}"  remote add  origin "${_gitlab_root}/${_repo_name}.git"
+    "${_git}"  remote add  bit    "${_bucket_root}/${_repo_name}.git"
+fi
 
 }
 
