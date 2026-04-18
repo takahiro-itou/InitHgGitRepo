1ed85efc9428e97f0c147d5f7e28e8250dbdbfc0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-04-18 18:03:47 +0900

    リファクタリング

--- normalize-commit.sh
+++ normalize-commit.sh
@@ -21,8 +21,10 @@ local  _script_dir=$(dirname "${_script_real_file}")
 
 source  "${_script_dir}/config"
 
-user_email=${USER_EMAIL:-"${user_email_default}"}
-user_name=${USER_NAME:-"${user_name_default}"}
+local  _git=${GIT:-"${git_bin_default}"}
+
+local  _user_email=${USER_EMAIL:-"${user_email_default}"}
+local  _user_name=${USER_NAME:-"${user_name_default}"}
 
 gitlab_hostname="gitlab.com${url_host_postname}"
 bucket_hostname="bucket.org${url_host_postname}"
@@ -30,11 +32,11 @@ bucket_hostname="bucket.org${url_host_postname}"
 
 ##########################################################################
 
-git config --local  'user.email'  "${user_email}"
-git config --local  'user.name'   "${user_name}"
+"${_git}"  config --local  'user.email'  "${_user_email}"
+"${_git}"  config --local  'user.name'   "${_user_name}"
 
-git filter-repo --email-callback "  return b'${user_email}'"
-git filter-repo --name-callback  "  return b'${user_name}'"
+"${_git}"  filter-repo  --email-callback "  return b'${_user_email}'"
+"${_git}"  filter-repo  --name-callback  "  return b'${_user_name}'"
 
 }
 
