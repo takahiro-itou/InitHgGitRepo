1bb4b5ce9c5fb8d0a83a05789ab03ffe6d30f659
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-04-18 17:43:35 +0900

    リファクタリング
    ローカル変数（にする予定の変数）の名前を変更し
    頭にアンダースコアを追加

--- init-hggit-repo.sh
+++ init-hggit-repo.sh
@@ -20,15 +20,15 @@ local  _script_dir=$(dirname "${_script_real_file}")
 
 source  "${_script_dir}/config"
 
-hg=${HG:-"${hg_bin_default}"}
-hg_opts=${HG_OPTS:-'--verbose'}
-git=${GIT:-"${git_bin_default}"}
+_hg=${HG:-"${hg_bin_default}"}
+_hg_opts=${HG_OPTS:-'--verbose'}
+_git=${GIT:-"${git_bin_default}"}
 
 user_email=${USER_EMAIL:-"${user_email_default}"}
 user_name=${USER_NAME:-"${user_name_default}"}
 
-gitlab_hostname="gitlab.com${url_host_postname}"
-bucket_hostname="bucket.org${url_host_postname}"
+_gitlab_hostname="gitlab.com${url_host_postname}"
+_bucket_hostname="bucket.org${url_host_postname}"
 
 
 ##
@@ -44,7 +44,7 @@ else
     proj_name=''
     url_prefix="${url_account_name}"
 fi
-hg_url_root="${url_prefix}-hggit"
+_hg_url_root="${url_prefix}-hggit"
 
 if [[ $# -ge 3 ]] ; then
     dir_name=$3
@@ -58,19 +58,19 @@ fi
 ##
 
 if [[ ! -d ${dir_name} ]] ; then
-    _gitlab_root="git+ssh://git@${gitlab_hostname}:${hg_url_root}"
-    ${hg}   clone  ${hg_opts}  ${_gitlab_root}/${repo_name}.git  ${dir_name}
-    ${git}  init   ${dir_name}
+    _gitlab_root="git+ssh://git@${_gitlab_hostname}:${_hg_url_root}"
+    ${_hg}   clone  ${_hg_opts}  ${_gitlab_root}/${repo_name}.git  ${dir_name}
+    ${_git}  init   ${dir_name}
 fi
 
 cat  "${_script_dir}/hgrc"  |  sed  \
     -e  "s/@REPOSITORY_NAME@/${repo_name}/g"    \
-    -e  "s/@URL_ROOT@/${hg_url_root}/g"         \
+    -e  "s/@URL_ROOT@/${_hg_url_root}/g"        \
     -e  "s/@DIRECTORY_NAME@/${dir_name}/g"      \
   >  ${dir_name}/.hg/hgrc
 
-_gitlab_root="git@${gitlab_hostname}:${url_prefix}"
-_bucket_root="git@${bucket_hostname}:${url_prefix}"
+_gitlab_root="git@${_gitlab_hostname}:${url_prefix}"
+_bucket_root="git@${_bucket_hostname}:${url_prefix}"
 
 pushd  "${dir_name}"    1>&2
 git config --local user.email "${user_email}"
